22a1b9380fa4fa045ad2def923c2c8bfd98de3ba
Constantin <constantin.werner@gmail.com>
2023-03-25 22:08:30 +0100

    Merge

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -19,15 +19,15 @@ def clear_submit():
     st.session_state["submit"] = False
 
 
-st.set_page_config(page_title="KnowledgeGPT", page_icon="📖", layout="wide")
-st.header("📖KnowledgeGPT")
+st.set_page_config(page_title="WissenGPT", page_icon="📖", layout="wide")
+st.header("📖WissenGPT")
 
 sidebar()
 
 uploaded_file = st.file_uploader(
-    "Upload a pdf, docx, or txt file",
+    "Lade ein pdf, docx oder txt file hoch",
     type=["pdf", "docx", "txt"],
-    help="Scanned documents are not supported yet!",
+    help="Keine Unterstützung von gescannten Dokumenten!",
     on_change=clear_submit,
 )
 
@@ -41,26 +41,26 @@ if uploaded_file is not None:
     elif uploaded_file.name.endswith(".txt"):
         doc = parse_txt(uploaded_file)
     else:
-        raise ValueError("File type not supported!")
+        raise ValueError("Dateityp unbekannt!")
     text = text_to_docs(doc)
     try:
-        with st.spinner("Indexing document... This may take a while⏳"):
+        with st.spinner("Lese dokument...⏳"):
             index = embed_docs(text)
         st.session_state["api_key_configured"] = True
     except OpenAIError as e:
         st.error(e._message)
 
-query = st.text_area("Ask a question about the document", on_change=clear_submit)
-with st.expander("Advanced Options"):
+query = st.text_area("Stell eine Frage zum Dokument", on_change=clear_submit)
+with st.expander("Extra-Optionen"):
     show_all_chunks = st.checkbox("Show all chunks retrieved from vector search")
     show_full_doc = st.checkbox("Show parsed contents of the document")
 
 if show_full_doc and doc:
-    with st.expander("Document"):
+    with st.expander("Dokument"):
         # Hack to get around st.markdown rendering LaTeX
         st.markdown(f"<p>{wrap_text_in_html(doc)}</p>", unsafe_allow_html=True)
 
-button = st.button("Submit")
+button = st.button("Los...")
 if button or st.session_state.get("submit"):
     if not st.session_state.get("api_key_configured"):
         st.error("Please configure your OpenAI API key!")
@@ -81,11 +81,11 @@ if button or st.session_state.get("submit"):
                 sources = get_sources(answer, sources)
 
             with answer_col:
-                st.markdown("#### Answer")
+                st.markdown("#### Antwort")
                 st.markdown(answer["output_text"].split("SOURCES: ")[0])
 
             with sources_col:
-                st.markdown("#### Sources")
+                st.markdown("#### Quellen")
                 for source in sources:
                     st.markdown(source.page_content)
                     st.markdown(source.metadata["source"])
--- knowledge_gpt/prompts.py
+++ knowledge_gpt/prompts.py
@@ -26,6 +26,39 @@ QUESTION: {question}
 =========
 FINAL ANSWER:"""
 
+
+## Use a shorter template to reduce the number of tokens in the prompt
+template2 = """
+Erzeuge eine endgültige Antwort zur den angegebenen Fragen mit Hilfe des angegebenen Dokuments als Referenz für Auszüge (in beliebiger Reihenfolge). 
+Füge immer einen "QUELLEN" Abschnitt in deiner Antwort einschliesslich einer minimalen Anzahl von QUELLEN, um die Frage zu beantworten. 
+Wenn Gesetzesangaben, Paragraphen, Verordnungen, Verweise genannt sind, gib sie immer auch an. 
+Wenn du die Frage nicht sicher beantworten kannst, sage dies und zeige auch keine QUELLE an. Antworten dürfen nicht ohne QUELLE fabriziert sein.
+
+FRAGE: Wie werden Gewinne/Verluste aus dem Krypto-Trading besteuert?
+=========
+Inhalt: Wird der An- und Verkauf von Kryptowährungen als Privatperson unternommen, sind § 22 Nr. 2, § 23 Abs. 1 Nr. 2 EStG einschlägig. Es handelt sich hierbei um ein privates Veräußerungsgeschäft von „anderen Wirtschaftsgütern“. Gemäß § 23 Abs. 3 Satz 1 EStG ist der Gewinn oder Verlust der Unterschied zwischen Veräußerungspreis einerseits und den Anschaffungs- und Werbungskosten andererseits. Es muss also nur der Anschaffungspreis vom Veräußerungspreis abgezogen werden. Die Gebühren beim Handel auf den Börsen sind Werbungskosten und damit abzugsfähig.
+Quelle: § 22 Nr. 2, § 23 Abs. 1 Nr. 2 EStG
+Inhalt: In § 23 Abs. 3 Satz 5 EStG ist zudem eine Freigrenze von 600 € vorgesehen, bis zu deren Erreichen alle privaten Veräußerungsgeschäfte des Veranlagungszeitraums steuerfrei bleiben. Wird die Grenze überschritten, muss allerdings der gesamte Betrag ab dem ersten Euro versteuert werden. Die Einkommensteuer fällt dabei nicht erst beim Umtausch von Kryptowährungen in Euro oder eine andere Fremdwährung an, sondern bereits bei einem Tausch in eine beliebige andere Kryptowährung oder auch beim Kauf von Waren oder Dienstleistungen mit einer solchen. Vergeht aber zwischen Anschaffung und Veräußerung mehr als ein Jahr, greift die Haltefrist des § 23 Abs. 1 Nr. 2 Satz 1 EStG. In diesen Fällen ist der gesamte Veräußerungsgewinn nicht steuerbar.
+Quelle: § 23 Abs. 3 Satz 5 EStG
+Inhalt: Zur Bestimmung der Anschaffungskosten und des Veräußerungsgewinns sowie zur Bestimmung der Einhaltung der Haltefrist wird in der Regel die sogenannte FIFO-Methode aus § 23 Abs. 1 Nr. 2 Satz 3 EStG herangezogen. Zwar schreibt das Gesetz diese First-In-First-Out-Methode nicht für Kryptowährungen vor, in der Praxis wird sie aber weitgehend angewendet. Auch das BMF hat sich für die Anwendung der FIFO-Methode innerhalb des BMF-Schreibens ausgesprochen. Es werden allerdings auch andere Meinungen vertreten und eine Berechnung nach der LIFO-Methode oder – zur Bestimmung der Anschaffungskosten – nach Durchschnittswerten vorgeschlagen.
+Wird der Handel mit Kryptowährungen gewerblich durchgeführt (z.B. durch Anbieten einer Webseite zum Umtausch von Kryptowährungen) sind die Erträge als solche aus Gewerbebetrieb gem. § 15 EStG zu versteuern. In diesem Rahmen gibt es insbesondere keine Haltefrist. Neben der Einkommensteuer muss in dem Fall auch Gewerbesteuer gezahlt werden. Allerdings sieht § 11 Abs. 1 Satz 3 Nr. 1 GewStG einen Freibetrag in Höhe von 24.500 Euro vor. Zudem hat sich der Händler mit Fragen der Umsatzsteuerbarkeit seiner Tätigkeit zu beschäftigen.
+Quelle: 1-31
+=========
+FINALE ANTWORT: Gewinne und Verluste aus Kryptowährungen gehören zu den Privaten Veräußerungsgeschäften, wie z.B. auch der Verkauf von Antiquitäten.
+Der Gewinn unterliegt nicht der Abgeltungssteuer, sondern wird mit dem persönlichen Steuersatz bei der Einkommensteuer versteuert.
+Es gibt eine Freigrenze von 600 Euro im Jahr, wird diese überschritten, ist für die gesamten Gewinne Steuer zu zahlen, nicht nur für den Betrag, der die Freigrenze überschreitet!
+Liegt zwischen Kauf und Verkauf mehr als ein Jahr, sind die Gewinne steuerfrei. Verluste können dann allerdings auch nicht angesetzt werden. Gewinne aus dem Verkauf von Kryptowährungen können mit Verlusten aus anderen privaten Veräußerungsgeschäften verrechnet werden.
+Es gibt eine Freigrenze von 600 Euro, alle Gewinne unter dieser Grenze sind steuerfrei. Durch geschickte Nutzung der FIFO- oder LIFO-Methode kann der Gewinn optimiert werden, falls im Laufe der Zeit mehrmals Bitcoins gekauft wurden.
+QUELLEN: § 22 Nr. 2, § 23 Abs. 1 Nr. 2 EStG, § 23 Abs. 3 Satz 5 EStG, 1-31
+---------
+
+FRAGE: {question}
+=========
+{summaries}
+=========
+FINALE ANTWORT:"""
+
 STUFF_PROMPT = PromptTemplate(
-    template=template, input_variables=["summaries", "question"]
+    template=template2, input_variables=["summaries", "question"]
 )
+
