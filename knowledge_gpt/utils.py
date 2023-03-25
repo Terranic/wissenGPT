9bb5f6bb602e5a4febf35f59d081957041c2a6d2
Terranic <59094334+Terranic@users.noreply.github.com>
2023-03-25 21:53:32 +0100

    Update utils.py
    again

--- knowledge_gpt/utils.py
+++ knowledge_gpt/utils.py
@@ -154,7 +154,8 @@ def get_sources(answer: Dict[str, Any], docs: List[Document]) -> List[Document]:
     return source_docs
 
 
-def wrap_text_in_html(text: str | List[str]) -> str:
+#def wrap_text_in_html(text: str | List[str]) -> str:
+def wrap_text_in_html(text: Union[str, List[str]]) -> str:    
     """Wraps each text block separated by newlines in <p> tags"""
     if isinstance(text, list):
         # Add horizontal rules between pages
